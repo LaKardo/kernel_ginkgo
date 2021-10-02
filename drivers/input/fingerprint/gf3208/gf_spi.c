f68829ea956396b448f6fcdd1c970ce569f4da3d
LaKardo <kishenko2017@gmail.com>
2021-10-02 22:28:27 +0300

    drivers: Adapt to new perf critical IRQs implementation

--- drivers/input/fingerprint/gf3208/gf_spi.c
+++ drivers/input/fingerprint/gf3208/gf_spi.c
@@ -212,7 +212,7 @@ static int inline irq_setup(struct gf_dev *gf_dev)
 	int status;
 	gf_dev->irq = gpio_to_irq(gf_dev->irq_gpio);
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_AFFINE,
 			"gf", gf_dev);
 
 	if (status) {
